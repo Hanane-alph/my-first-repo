b919f3a804d72a78be935d3f5e956ebcd80f0e1b
HANANE <bolton915fersa@gmail.com>
2025-01-15 21:12:37 -0500

    first run

---
+++ lesson01/HalloWorld.java
@@ -0,0 +1,14 @@
+package com.neotech.lesson01;
+
+public class HalloWorld {// this marks the begnning of the class
+
+	// to save changes use: CTRL + S
+	// the main method is the starting point to run a java application
+	public static void main(String[] args) {
+
+		// CTRL + SHIFT + F--> formats the code automatically
+System.out.println("HalloWorld");//the first instruction we are writing
+	  System.out.println("how are you doing???");
+	}// this is the end of the method
+
+}// this is the end of class
---
+++ lesson01/testclass.java
@@ -0,0 +1,14 @@
+package com.neotech.lesson01;
+
+public class testclass {
+public static void main(String[] args) {
+	//I can write anything in this line because its a comment
+
+System.out.println("here i can write anything, java with print it out!!");
+}
+
+
+	
+	
+	
+}
